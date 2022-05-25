53eb997c8d6b7b8ce4ccc5bb9f0a7ac1af1c0ddc
Lucas Passos <lucasplpx@gmail.com>
2022-05-25 20:50:31 -0300

    feat: config inicial

---
+++ App.tsx
@@ -0,0 +1,65 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Home } from './src/screens/Home';
+
+import * as SplashScreen from 'expo-splash-screen';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+
+import theme from './src/styles/theme';
+import { View } from 'react-native';
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        // fontsLoaded
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </View>
+  );
+}
