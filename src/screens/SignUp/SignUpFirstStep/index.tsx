d86d2e3c9357c3b24c4a07072fd22c817a8a5010
Lucas Passos <lucasplpx@gmail.com>
2022-06-23 19:53:45 -0300

    primeiro step

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,7 +1,14 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
+import { Button } from '../../../components/Button';
+import { Input } from '../../../components/Input';
 
 import {
   Container,
@@ -21,20 +28,28 @@ export const SignUpFirstStep = () => {
   }
 
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
-      <Title>Crie sua{'\n'}conta</Title>
-      <SubTitle>Faça seu cadastro de{'\n'}forma rápida e fácil</SubTitle>
+    <KeyboardAvoidingView behavior='position' enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
+          <Title>Crie sua{'\n'}conta</Title>
+          <SubTitle>Faça seu cadastro de{'\n'}forma rápida e fácil</SubTitle>
 
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input iconName='user' placeholder='Nome' />
+            <Input iconName='mail' placeholder='E-mail' />
+            <Input iconName='credit-card' placeholder='CNH' />
+          </Form>
+          <Button title='Próximo' />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
--- src/screens/SignUp/SignUpFirstStep/styles.ts
+++ src/screens/SignUp/SignUpFirstStep/styles.ts
@@ -3,7 +3,6 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  flex: 1;
   padding: 0 24px;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
@@ -44,8 +43,10 @@ export const Form = styled.View`
   margin-top: 64px;
   margin-bottom: 16px;
 `;
+
 export const FormTitle = styled.Text`
   font-size: ${RFValue(20)}px;
   font-family: ${({ theme }) => theme.fonts.secondary_600};
   color: ${({ theme }) => theme.colors.title};
+  margin-bottom: 24px;
 `;
